cebb4537ae2c3672b4793436e2a52b746996760e
Kaitlyn Clay <kclay@students.mchenry.edu>
2024-02-07 09:10:55 -0600

    error

--- app/src/main/java/com/example/topappbar/MainActivity.kt
+++ app/src/main/java/com/example/topappbar/MainActivity.kt
@@ -6,20 +6,15 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.widget.SearchView
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
 
-        val appBarConfiguration = AppBarConfiguration(setOf(R.id.navigation_home,
-            R.id. navigation_dashboard, R.id.navigation_notifications))
-        binding.myToolbar.setupWithNavController(navController, appBarConfiguration)
+        // up/back button - home navigation enabled/disabled
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         setSupportActionBar(findViewById(R.id.my_toolbar))
 
@@ -43,40 +38,36 @@ class MainActivity : AppCompatActivity() {
         }
     }
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.main_activity_actions, menu)
+        menuInflater.inflate(R.menu.menu_res, menu)
 
         val searchItem = menu?.findItem(R.id.action_search)
         val searchView = searchItem?.actionView as SearchView
 
         // Configure the search info and add any event listeners.
 
-        return super.onCreateOptionsMenu(menu)
-    }
-//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-//        menuInflater.inflate(R.menu.options, menu)
-//
-//        // Define the listener.
-//        val expandListener = object : MenuItem.OnActionExpandListener {
-//            override fun onMenuItemActionCollapse(item: MenuItem): Boolean {
-//                // Do something when the action item collapses.
-//                return true // Return true to collapse the action view.
-//            }
-//
-//            override fun onMenuItemActionExpand(item: MenuItem): Boolean {
-//                // Do something when it expands.
-//                return true // Return true to expand the action view.
-//            }
-//        }
-//
-//        // Get the MenuItem for the action item.
-//        val actionMenuItem = menu?.findItem(R.id.myActionItem)
-//
-//        // Assign the listener to that action item.
-//        actionMenuItem?.setOnActionExpandListener(expandListener)
-//
-//        // For anything else you have to do when creating the options menu,
-//        // do the following:
-//
-//        return true
-//    }
+        //Define the listener.
+        val expandListener = object : MenuItem.OnActionExpandListener {
+            override fun onMenuItemActionCollapse(item: MenuItem): Boolean {
+                // Do something when the action item collapses.
+                return true // Return true to collapse the action view.
+            }
+
+            override fun onMenuItemActionExpand(item: MenuItem): Boolean {
+                // Do something when it expands.
+                return true // Return true to expand the action view.
+            }
+        }
+
+        // Get the MenuItem for the action item.
+        val actionMenuItem = menu?.findItem(R.id.action_settings)
+
+        // Assign the listener to that action item.
+        actionMenuItem?.setOnActionExpandListener(expandListener)
+
+        // For anything else you have to do when creating the options menu,
+        // do the following:
+
+        return true
+            }
+
 }
